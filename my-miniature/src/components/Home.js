9b0c892e95077ceb5cbaf06c9363a046ffd5a4ab
Glen <glen.kimani@student.moringaschool.com>
2023-03-02 09:19:51 +0300

    homepage updated

---
+++ my-miniature/src/components/Home.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import MiniatureCard from './MiniatureCard';
+
+function Home() {
+    const[ newMinis, setNewMinis ] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:9292/miniatures/new')
+        .then(res => res.json())
+        .then(data => setNewMinis(data))
+    }, [])
+
+    const miniatureCards = newMinis.map((miniature, index) => <MiniatureCard key={ index } miniature={ miniature }/>)
+
+    return (
+        <div className="set-page">
+            <div className="quote"><b>
+                <p><i></i></p></b>
+                <p>- </p></div>
+            <h3>Latest miniatures in your collection:</h3>
+            <div className="card-grid">{ miniatureCards }</div>
+        </div>
+    )
+}
+
+export default Home;
+ 
